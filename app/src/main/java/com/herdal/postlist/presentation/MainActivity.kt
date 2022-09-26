3511e9ebda90a7cd4a8e8fda18a6f2ebb0443203
herdal06 <h.erdal721@gmail.com>
2022-09-26 17:22:13 +0300

    Setup navigation and add recycler view into home page

---
+++ app/src/main/java/com/herdal/postlist/presentation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.herdal.postlist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.herdal.postlist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
