4886175352981263401945a6eaef1bdc841bda22
herdal06 <h.erdal721@gmail.com>
2022-09-27 15:19:48 +0300

    Design user details layout

---
+++ app/src/main/java/com/herdal/postlist/presentation/user_details/UserDetailsFragment.kt
@@ -0,0 +1,31 @@
+package com.herdal.postlist.presentation.user_details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.herdal.postlist.databinding.FragmentUserDetailsBinding
+
+class UserDetailsFragment : Fragment() {
+
+    private var _binding: FragmentUserDetailsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUserDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
