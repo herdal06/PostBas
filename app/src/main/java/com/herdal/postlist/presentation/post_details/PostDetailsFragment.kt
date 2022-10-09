7a8b775d251786838349e76d7eae443a2aaf9b53
herdal06 <h.erdal721@gmail.com>
2022-10-09 16:57:01 +0300

    Disable textview maxLines and ellipsize

--- app/src/main/java/com/herdal/postlist/presentation/post_details/PostDetailsFragment.kt
+++ app/src/main/java/com/herdal/postlist/presentation/post_details/PostDetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -114,12 +115,14 @@ class PostDetailsFragment : Fragment() {
 
     private fun manageUI() = binding.apply {
         includePostItem.apply {
-            tvPostBody.apply {
-                // disable maxLines and ellipsize
-                this.maxLines = Int.MAX_VALUE
-                this.ellipsize = null
-            }
+            disableMaxLinesAndEllipsize(tvPostTitle)
+            disableMaxLinesAndEllipsize(tvPostBody)
         }
+    }
+
+    private fun disableMaxLinesAndEllipsize(textView: TextView) = textView.apply {
+        this.maxLines = Int.MAX_VALUE
+        this.ellipsize = null
     }
 
     private fun getPostById() {
